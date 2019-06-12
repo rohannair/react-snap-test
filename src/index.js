1dfe714dfb614191c613d09f57a056e4f8a11b9c
Rohan Nair <r@rohannair.ca>
2019-06-12 08:45:26 -0400

    Adding react-snap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById("root");
+
+if (root.hasChildNodes()) {
+  hydrate(<App />, root);
+} else {
+  render(<App />, root);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
